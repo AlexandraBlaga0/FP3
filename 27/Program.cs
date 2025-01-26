99e4507c1f76e5c6a13cc17958d00d53b5fd437f
AlexandraBlaga0 <ablaga426@gmail.com>
2025-01-26 23:58:31 +0200

    Add files via upload

---
+++ 27/Program.cs
@@ -0,0 +1,43 @@
+﻿namespace _27
+{
+    internal class Program
+    {
+        static void SelectionSort(int n, int[]v)
+        {
+            for (int i = 0; i < n - 1; i++)
+            {
+                int indexMin=i;
+                for (int j = i + 1; j < n; j++)
+                    if (v[j] < v[indexMin])
+                    {
+                        indexMin = j;
+                    }
+                if (indexMin!=i)
+                {
+                    int aux = v[i];
+                    v[i] = v[indexMin];
+                    v[indexMin] = aux;
+                }
+            }
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Dimensiune vector:");
+            int n = int.Parse(Console.ReadLine());
+            Console.WriteLine("Elemente vector:");
+            int[] v = new int[n];
+
+            for (int i = 0; i < n; i++)
+                v[i] = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("index vector");
+            int k = int.Parse(Console.ReadLine());
+            Console.WriteLine("Valoarea elementului de pe k este " + v[k]);
+
+            SelectionSort(n, v);
+            Console.WriteLine("Acum valoarea elementului de pe k este " + v[k]);
+
+
+        }
+    }
+}
---
+++ 30/Program.cs
@@ -0,0 +1,51 @@
+﻿namespace _30
+{
+    internal class Program
+    {
+        static void SelectionSort(int n, int[]E, int[]W)
+        {
+            for(int i=0; i<n-1; i++)
+            {
+               int  indexMin = i;
+                for(int j=i+1; j<n; j++)
+                {
+                    if (E[j] < E[indexMin])
+                        indexMin = j;
+                    else if (E[j] == E[indexMin] && W[j] > W[indexMin])
+                        indexMin = j;
+                }
+                if (indexMin != i)
+                {
+                   int aux = E[i];
+                    E[i]= E[indexMin];
+                    E[indexMin] = aux;
+                    aux = W[i];
+                    W[i] = W[indexMin];
+                    W[indexMin] = aux;
+                }
+            }
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Dimensiune vectori :");
+            int n = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Elemente E:");
+            int[] E = new int[n];
+            for (int i = 0; i < n; i++)
+                E[i] = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Elemente W:");
+            int[] W = new int[n];
+            for (int i = 0; i < n; i++)
+                W[i] = int.Parse(Console.ReadLine());
+
+            
+            SelectionSort(n, E, W);
+
+            for (int i = 0; i < n; i++)
+                Console.WriteLine($"Valoare E: {E[i]}, Valoare W: {W[i]}");
+
+        }
+    }
+}
